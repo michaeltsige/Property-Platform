03f8c0c369ee1b65a232e293a8278bf02c9d3c86
Michael <michaeltsige11@gmail.com>
2026-01-18 19:58:40 +0300

    feat: added authorization middleware

--- backend/src/middleware/Auth.js
+++ backend/src/middleware/Auth.js
@@ -33,6 +33,22 @@ const protect = async (req, res, next)  => {
   } catch (error) {
     console.error('Auth middleware error:', error);
     return res.status(401).json({ error: 'Not authorized, token failed' });
-  }
+  };
 
-}
+};
+
+const authorize = (...roles) => {
+    return (req, res, next) => {
+        if(!req.user) {
+            return res.status(401).json({ error: 'not authenticated'});
+        }
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ error: 'forbidden'});
+        }
+
+        next();
+    };
+};
+
+module.exports = { protect, authorize };
